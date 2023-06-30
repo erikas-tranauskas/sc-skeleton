53742714fba2c8d935e88df6b06225971b50f6cd
erikas.tranauskas <erikas.tranauskas@pigu.lt>
2023-06-30 18:02:17 +0300

    Test deployment and set messages

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,16 +2,34 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
+
+const INITIAL_MESSAGE = 'first initial message';
 
 let accounts = [];
+let inbox;
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
-})
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
+        .send({ from: accounts[0], gas: 1000000 });
+});
 
 describe('Inbox', () => {
-    it('contract deployment', () => {
-        console.log(accounts);
-        assert.equal(true, true)
-    })
+    it('contract deployment success', () => {
+        assert.ok(inbox.options.address);
+    });
+
+    it('initial message check', async () => {
+        assert.equal(await inbox.methods.message().call(), INITIAL_MESSAGE);
+    });
+
+    it('set message check', async () => {
+        const expected = 'test message';
+        await inbox.methods.setMessage(expected).send({ from: accounts[0] });
+
+        assert.equal(expected, await inbox.methods.message().call());
+    });
 });
