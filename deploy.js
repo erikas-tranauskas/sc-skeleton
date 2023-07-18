2662d14e85702de6bc7c8d70c39da309a6095729
erikas.tranauskas <erikas.tranauskas@pigu.lt>
2023-07-18 18:24:06 +0300

    SC deploy in progress

--- deploy.js
+++ deploy.js
@@ -1,1 +1,10 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'seed phrase goes in here',
+    'goerly-url'
+);
+
+const web3 = new Web3(provider);
