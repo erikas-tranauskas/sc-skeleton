7a4c2c182122a916882af74e3b56d9009784c2f3
erikas.tranauskas <erikas.tranauskas@pigu.lt>
2023-07-19 18:08:28 +0300

    SC deploy in progress

--- deploy.js
+++ deploy.js
@@ -3,8 +3,21 @@ const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
-    'seed phrase goes in here',
-    'goerly-url'
+    'corn bullet knee girl gym ugly border wide program fresh hollow body',
+    'https://goerli.infura.io/v3/ce83748745ea48ccbab0446d99a4af0e'
 );
 
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Message'] })
+        .send({ from: accounts[0], gas: 1000000 });
+
+    console.log(result.options.address);
+    provider.engine.stop();
+};
+
+deploy();
