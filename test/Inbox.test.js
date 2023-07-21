6eb5ab2a7ddca38d558e8c3ad45d9439c9419cf9
erikas.tranauskas <erikas.tranauskas@pigu.lt>
2023-07-21 16:38:45 +0300

    Solidity version update

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,35 +1,36 @@
 const assert = require('assert');
-const ganache = require('ganache-cli');
-const Web3 = require('web3');
+const ganache = require('ganache');
+const { Web3 } = require('web3');
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require('../compile');
 
-const INITIAL_MESSAGE = 'first initial message';
+const { abi, evm } = require('../compile');
 
-let accounts = [];
+let accounts;
 let inbox;
 
 beforeEach(async () => {
-    accounts = await web3.eth.getAccounts();
-
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
-        .send({ from: accounts[0], gas: 1000000 });
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({
+      data: evm.bytecode.object,
+      arguments: ['Hi there!'],
+    })
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
-    it('contract deployment success', () => {
-        assert.ok(inbox.options.address);
-    });
+  it('contract deployment success', () => {
+    assert.ok(inbox.options.address);
+  });
 
-    it('initial message check', async () => {
-        assert.equal(await inbox.methods.message().call(), INITIAL_MESSAGE);
-    });
+  it('initial message check', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
 
-    it('set message check', async () => {
-        const expected = 'test message';
-        await inbox.methods.setMessage(expected).send({ from: accounts[0] });
-
-        assert.equal(expected, await inbox.methods.message().call());
-    });
+  it('set message check', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
+  });
 });
